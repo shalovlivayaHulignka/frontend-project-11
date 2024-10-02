7aeaf02ced6a80565c17e5a7dc85053aca6bab16
Mamokko <shalovlivaya.huliganka@gmail.com>
2024-10-02 23:31:54 +0500

    Edit the webpack configuration file

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'url';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const filename = fileURLToPath(import.meta.url);
+const dirName = dirname(filename);
 
 export default {
   mode: process.env.NODE_ENV || 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(dirname('dist')),
+    path: path.resolve(dirName, 'dist')
   },
   devServer: {
     open: true,
@@ -18,6 +22,9 @@ export default {
       template: 'index.html',
     }),
   ],
+  performance: {
+    hints: false,
+  },
   module: {
     rules: [
       { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
