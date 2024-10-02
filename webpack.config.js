ebae55ed8d985d9d0bf183e8eabcd5e440c2c6b5
Mamokko <shalovlivaya.huliganka@gmail.com>
2024-10-02 16:35:56 +0500

    Add saas

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 import path, { dirname } from 'node:path';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
@@ -12,25 +13,21 @@ export default {
     host: 'localhost',
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
   ],
   module: {
     rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
-        test: /\.(js|jsx)$/i,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset',
-      },
-      {
-        test: /\.css$/,
+        test: /\.scss$/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
+          'postcss-loader',
+          'sass-loader',
         ],
       },
     ],
