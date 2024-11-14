e89402faf4daadd7ea0c5ee8977d4d35565bebd9
Mamokko <shalovlivaya.huliganka@gmail.com>
2024-11-14 13:44:00 +0500

    Add a display feeds

--- src/view.js
+++ src/view.js
@@ -4,8 +4,43 @@ export default (state, i18nextInstance) => {
   const form = document.querySelector('.rss-form'),
     input = document.querySelector('#url-input'),
     feedback = document.querySelector('.feedback'),
-    button = document.querySelector('[type="submit"]');
+    button = document.querySelector('[type="submit"]'),
+    posts = document.querySelector('.posts'),
+    feeds = document.querySelector('.feeds');
 
+  const renderFeeds = () => {
+    const card = document.createElement('div');
+    card.classList.add('card', 'border-0');
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
+    const cardTitle = document.createElement('h2');
+    cardTitle.classList.add('card-title', 'h4');
+
+    cardTitle.textContent = i18nextInstance.t('feeds');
+    cardBody.append(cardTitle);
+    card.append(cardBody);
+
+    const ul = document.createElement('ul');
+    ul.classList.add('list-group', 'border-0', 'rounded-0');
+    const feedsElements = state.feeds.map((feed) => {
+      const li = document.createElement('li');
+      li.classList.add('list-group-item', 'border-0', 'border-end-0');
+      const h3 = document.createElement('h3');
+      h3.classList.add('h6', 'm-0');
+      h3.textContent = feed.title;
+      const p = document.createElement('p');
+      p.classList.add('m-0', 'small', 'text-black-50');
+      p.textContent = feed.description;
+      li.append(h3, p);
+
+      return li;
+    });
+    ul.append(...feedsElements);
+    card.append(ul);
+    feeds.append(card);
+  };
 
   const watchedState = onChange(state, (path, value) => {
     if (path === 'processState') {
@@ -32,6 +67,10 @@ export default (state, i18nextInstance) => {
         feedback.textContent = i18nextInstance.t('success');
         feedback.classList.remove('text-danger');
         feedback.classList.add('text-success');
+        posts.innerHTML = '';
+        feeds.innerHTML = '';
+        // renderPosts(watchedState);
+        renderFeeds(watchedState);
         watchedState.processState = 'finished';
       }
     }
