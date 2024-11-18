0999f12901c6d12642f1a8cd35fce8f288a8aec4
Mamokko <shalovlivaya.huliganka@gmail.com>
2024-11-18 16:12:34 +0500

    Add timer and refactor

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 import * as yup from 'yup';
 import _ from 'lodash';
 import i18next from 'i18next';
+import { setLocale } from 'yup';
 import view from './view.js';
 import languages from './locales/index.js';
 import axios from 'axios';
+
+const addPost = (data) => {
+  const feedId = _.uniqueId();
+  const { name, description } = data.feed;
+  const feed = { feedId, name, description };
+  const posts = data.posts.map((post) => ({ feedId, id: _.uniqueId(), ...post }));
+  return {feed, posts };
+}
 
 const getURL = (url) => {
   const result = new URL('/get', 'https://allorigins.hexlet.app');
@@ -38,17 +47,25 @@ const parseData = (data) => {
 };
 
 const loadUrl = (url) => axios.get(getURL(url))
-  .then((response) => parseData(response.data.contents))
-  .catch((e) => console.log(e.message));
+  .catch(() => throw new Error('errors.requestErr'))
+  .then((response) => {
+    const parse = parseData(response.data.contents);
+    return addPost(parse);
+  })
+  .catch((e) => throw new Error(e.message));
 
 const app = () => {
   const defaultLanguage = 'ru';
   const i18nextInstance = i18next.createInstance();
   i18nextInstance.init({
-    lng: 'ru',
-    debug: true,
+    lng: defaultLanguage,
+    debug: false,
     resources: languages,
-  });
+  })
+    .then(setLocale({
+      mixed: { notOneOf: 'errors.existedUrl' },
+      string: { url: 'errors.invalidUrl' },
+    }));
 
   const state = {
     lng: defaultLanguage,
@@ -66,13 +83,35 @@ const app = () => {
   const status = view(state, i18nextInstance);
   const postsContainer = document.querySelector('.posts');
   postsContainer.addEventListener('click', (e) => {
-    // console.log(status);
     const { id } = e.target.dataset;
     if (!id) return;
     status.id.add(id);
-    const { title, description, link } = status.posts[0].filter((item) => item.id === id)[0];
+    // const { title, description, link } = status.posts[0].filter((item) => item.id === id)[0];
+    const { title, description, link } = status.posts.filter((item) => item.id === id)[0];
     status.modal = { title, description, link };
   });
+
+  const updateRSS = () => {
+    const promises = status.links
+      .map((link, index) => loadUrl(link)
+        .then((response) => {
+          const { feedId } = status.feeds[index];
+          const filterPosts = status.posts.filter((post) => post.feedId === feedId);
+          const currentNewPosts = _.differenceBy(response.posts, filterPosts, 'title')
+            .map((post) => ({ feedId, id: _.uniqueId, ...post }));
+          if (currentNewPosts.length > 0) {
+            status.posts.push(...currentNewPosts);
+            status.processState = 'processed';
+          }
+        })
+        .catch((err) => {
+          status.error = err.message;
+          status.processState = 'failed';
+          throw new Error(err.message);
+        }));
+    Promise.all(promises).finally(() => setTimeout(updateRSS, 5000));
+  };
+  setTimeout(updateRSS, 5000);
 
   const form = document.querySelector('.rss-form');
   const schema = yup.string().url().required();
@@ -87,12 +126,9 @@ const app = () => {
         return loadUrl(link);
       })
       .then((data) => {
-        const feedId = _.uniqueId();
-        const { name, description } = data.feed;
-        const feed = { feedId, name, description };
-        const posts = data.posts.map((post) => ({ feedId, id: _.uniqueId(), ...post }));
-        status.feeds.push(feed);
-        status.posts.push(posts);
+        status.feeds.push(data.feed);
+        status.posts.push(...data.posts);
+        status.links.push(link);
         status.processState = 'processed';
       })
       .catch((error) => {
